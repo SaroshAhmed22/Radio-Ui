81b526d866b95f61eb5b4bdbe9e4c2e7dc191872
SaroshAhmed22 <SaroshAhmed622@gmail.com>
2021-08-09 03:39:54 +0500

    text editing

--- js/slider2.js
+++ js/slider2.js
@@ -25,3 +25,39 @@ $(document).ready(function ($) {
         }]
     });
 });
+
+// ! SLider 3
+
+$(document).ready(function ($) {
+    $('.card-slider1').slick({
+        dots: false,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: false,
+        autoplaySpeed: 2000,
+        arrows: true,
+        responsive: [{
+            breakpoint: 600,
+            settings: {
+
+
+
+                slidesToShow: 2,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 400,
+            settings: {
+                dots: false,
+                arrows: true,
+
+
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }]
+    });
+});
